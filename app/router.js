505cb2065bcfe45f9a7e86e89cbcbc6df1c5a5bb
Matt Payne <Payne@MattPayne.org>
2016-11-11 20:28:34 -0600

    /quotes and /quote/2 both work

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('quotes');
-  this.route('quote');
+  this.route('quote', { path: '/quotes/:quote_id'});
 });
 
 export default Router;
--- app/routes/quote.js
+++ app/routes/quote.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return [
+      {id: '1', quote: 'I like eggs'},
+      {id: '2', quote: 'No matter where you go, there you are!'}
+    ].findBy('id', params.quote_id);
+  }
 });
